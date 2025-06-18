7f46b964acb8d8fed536fb3f16b2543cf1d94aee
nabeelkhancs <34935048+nabeelkhancs@users.noreply.github.com>
2025-06-18 15:40:18 +0500

    error handling change

--- src/config/errorHandler.ts
+++ src/config/errorHandler.ts
@@ -1,19 +1,24 @@
 import { AxiosError } from "axios";
 import Cookies from "js-cookie";
 export const errorHandler = (error: AxiosError | any): string => {
-    if (error?.response) {
-        if (error.response.status === 401) {
-            setTimeout(() => {
-                Cookies.remove('token');
-                if (typeof window !== 'undefined' && !window.location.pathname.includes('login') && !window.location.pathname.includes('verification')) {
-                    window.location.replace('/admin/login')
-                }
-            }, 1000);
+  const userType = Cookies.get('userType') || 'ADMIN';
+  if (error?.response) {
+    if (error.response.status === 401) {
+      setTimeout(() => {
+        Cookies.remove('token');
+        if (typeof window !== 'undefined' && !window.location.pathname.includes('login') && !window.location.pathname.includes('verification')) {
+          if (userType === 'ADMIN') {
+            window.location.replace('/admin/login')
+          } else {
+            window.location.replace('/login')
+          }
         }
-        return error.response.data?.message || 'Unauthorized';
-    } else if (error?.request) {
-        return error.message || 'No response from server';
-    } else {
-        return 'Something went wrong';
+      }, 1000);
     }
+    return error.response.data?.message || 'Unauthorized';
+  } else if (error?.request) {
+    return error.message || 'No response from server';
+  } else {
+    return 'Something went wrong';
+  }
 };
