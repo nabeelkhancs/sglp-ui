7a34de060c9695b9cc096b053a5f202e682f2439
Nabeel Khan <nabeel.khan@staples.ca>
2025-07-25 17:07:37 +0500

    fix

--- src/app/cases/[id]/page.tsx
+++ src/app/cases/[id]/page.tsx
@@ -5,13 +5,20 @@ import { courtData } from "@/utils/courtData";
 import CasesContainer from "@/containers/cases";
 import { useParams } from "next/navigation";
 import { useState } from "react";
+import { Button } from "antd";
 
 const CaseTypePage = () => {
+
   const params = useParams();
   const idParam = params?.id;
   const id = Array.isArray(idParam) ? idParam[0] : idParam;
   const [showTable, setShowTable] = useState(false);
   const [pageName, setPageName] = useState("");
+
+  const handleBack = () => {
+    setShowTable(false);
+  };
+
   if ((id == 'supremecourt' || id == 'highcourt' || id == 'districtcourts') && !showTable) {
     return (
       <DashboardLayout>
@@ -38,6 +45,11 @@ const CaseTypePage = () => {
   }
   return (
     <DashboardLayout>
+      {showTable &&
+        <div className="mb-3">
+          <Button onClick={handleBack} className="bg-transparent p-0 border-0 w-auto d-flex align-items-center gap-1  text-dark fw-medium">
+            <span><img src="/icons/chevron-left-black.svg" alt="Back" /></span>Back</Button>
+        </div>}
       <CasesContainer caseType={id} pageName={pageName} />
     </DashboardLayout>
   );
