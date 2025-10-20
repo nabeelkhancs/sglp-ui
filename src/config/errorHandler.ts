d1604fdd5cd97acaf7acb60f391de9af017f7214
Nabeel Khan <nabeel.khan@staples.ca>
2025-10-20 23:43:52 +0500

    changes

--- src/config/errorHandler.ts
+++ src/config/errorHandler.ts
@@ -1,9 +1,6 @@
 import { AxiosError } from "axios";
 import Cookies from "js-cookie";
 export const errorHandler = (error: AxiosError | any): string => {
-  if(error?.response?.data?.message && error.response.status === 401 && window.location.pathname.includes('login')) {
-    return error.response.data.message;
-  }
   const userType = Cookies.get('userType') || 'ADMIN';
   if (error?.response) {
     if (error.response.status === 401) {
@@ -18,7 +15,7 @@ export const errorHandler = (error: AxiosError | any): string => {
         }
       }, 1000);
     }
-    return error.response.data || 'Unauthorized';
+    return error.response.data?.message || 'Unauthorized';
   } else if (error?.request) {
     return error.message || 'No response from server';
   } else {
