a89484fdb2d9827c0e98245a310a906b0d7de029
Nabeel Khan <nabeel.khan@staples.ca>
2025-10-22 02:53:40 +0500

    fix error

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import ResetPasswordContainer from "@/containers/reset-password";
 import AuthLayout from "../layouts/AuthLayout";
 
 const ResetPasswordPage = () => {
-
-  return <AuthLayout>
-    <ResetPasswordContainer />;
-  </AuthLayout>
+  return (
+    <AuthLayout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordContainer />
+      </Suspense>
+    </AuthLayout>
+  );
 };
 
 export default ResetPasswordPage;
