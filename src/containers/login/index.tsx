f5b95b305a926ddc419caead17a33b1d5749d654
Nabeel Khan <nabeel.khan@staples.ca>
2025-10-23 19:07:49 +0500

    changes highliter

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -83,7 +83,7 @@ const LoginContainer = () => {
                 onChange={e => setPassword(e.target.value)}
                 iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
                 status={passwordError ? "error" : ""}
-                style={{ color: !passwordError ? "#fff" : '#000'}}
+                style={{ color: "#fff", background: !passwordError ? 'transparent' : '' }}
               />
               {passwordError && (
                 <div style={{ color: "#ff4d4f", fontSize: "13px", marginTop: "4px", background: "transparent", padding: 0 }}>{passwordError}</div>
